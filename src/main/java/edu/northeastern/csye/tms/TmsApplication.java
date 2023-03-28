88281a8ccae3e1e3542e4aa5678101473663c55c
Goutham K <kanahasabai.g@northeastern.edu>
2023-03-27 21:50:30 -0400

    feat: initial commit

---
+++ src/main/java/edu/northeastern/csye/tms/TmsApplication.java
@@ -0,0 +1,13 @@
+package edu.northeastern.csye.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsApplication.class, args);
+	}
+
+}
