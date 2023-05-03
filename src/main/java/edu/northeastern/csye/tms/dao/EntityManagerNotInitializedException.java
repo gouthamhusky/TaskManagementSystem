7d27271409a0fff409a26195dd3d566ddfea02cf
Goutham K <kanahasabai.g@northeastern.edu>
2023-05-03 16:47:41 -0400

    chore: added swagger and jaeger configs

---
+++ src/main/java/edu/northeastern/csye/tms/dao/EntityManagerNotInitializedException.java
@@ -0,0 +1,7 @@
+package edu.northeastern.csye.tms.dao;
+
+public class EntityManagerNotInitializedException extends Throwable {
+    public EntityManagerNotInitializedException(String message) {
+        super(message);
+    }
+}
