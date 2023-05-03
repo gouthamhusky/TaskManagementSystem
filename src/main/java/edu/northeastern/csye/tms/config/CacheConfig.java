57790b9ec211b0a2fe09a02461d5976957f1f548
Goutham K <kanahasabai.g@northeastern.edu>
2023-05-03 16:46:14 -0400

    feat: enabled hibernate level caching

---
+++ src/main/java/edu/northeastern/csye/tms/config/CacheConfig.java
@@ -0,0 +1,20 @@
+package edu.northeastern.csye.tms.config;
+
+import java.util.List;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setCacheNames(List.of("roles", "users", "pulses", "tasks"));
+        return cacheManager;
+    }
+}
--- src/main/java/edu/northeastern/csye/tms/service/UserRoleService.java
+++ src/main/java/edu/northeastern/csye/tms/service/UserRoleService.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ This class represents a service that handles User and Role objects and their respective persistenc
+ @author Goutham K
+ */
 @Service
 public class UserRoleService {
 
@@ -22,43 +26,102 @@ public class UserRoleService {
         this.roleDAO = roleDAO;
     }
 
+    /**
+     Persists a Role object.
+     @param role the Role to be persisted
+     */
     public void persistRole(Role role) {
         roleDAO.persist(role);
     }
 
+    /**
+     Updates a Role object.
+     @param role the Role to be updated
+     */
     public void updateRole(Role role) {
         roleDAO.update(role);
     }
 
+    /**
+     Deletes a Role object with the given id.
+     @param id the id of the Role to be deleted
+     */
     public void deleteRole(Integer id) {
         roleDAO.delete(id);
     }
 
+    /**
+     Retrieves a list of all Role objects.
+     @return a list of all Role objects
+     */
     public List<Role> getRoles() {
         return roleDAO.getRoles();
     }
 
+    /**
+     Retrieves a Role object with the given id.
+     @param roleId the id of the Role to be retrieved
+     @return the Role object with the given id
+     */
     public Role getRole(Integer roleId) {
         return roleDAO.get(roleId);
     }
 
+    /**
+     Persists a User object.
+     @param user the User to be persisted
+     */
     public void persistUser(User user) {
         userDAO.persist(user);
     }
 
+    /**
+     Updates a User object.
+     @param user the User to be updated
+     */
     public void updateUser(User user) {
         userDAO.update(user);
     }
 
+    /**
+     Deletes a User object with the given id.
+     @param id the id of the User to be deleted
+     */
     public void deleteUser(Integer id) {
         userDAO.delete(id);
     }
 
+    /**
+     Retrieves a list of all User objects.
+     @return a list of all User objects
+     */
     public List<User> getUsers() {
         return userDAO.getUsers();
     }
 
+    /**
+     Retrieves a User object with the given id.
+     @param userId the id of the User to be retrieved
+     @return the User object with the given id
+     */
     public User getUser(Integer userId) {
         return userDAO.get(userId);
+    }
+
+    /**
+     Retrieves a User object with the given username.
+     @param userName the username of the User to be retrieved
+     @return the User object with the given username
+     */
+    public User getUser(String userName) {
+        return userDAO.get(userName);
+    }
+
+    /**
+     Retrieves the number of User objects.
+     @return the number of User objects
+     */
+    public int getUserCount(){
+        return userDAO.getUserCount();
     }
 }
